56d035dc2fe1f5b13d0aa21b4b8c350cb1bd5564
Amilie <amilie@allthingslinux.com>
2024-11-28 11:34:01 +0000

    Update Main.java
    Made code cleaner and easier to use.

--- Main.java
+++ Main.java
@@ -9,59 +9,65 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("[NOTIFICATION] Enter your query: ");
-            String userInput = scanner.nextLine();
-            scanner.close();
-
+            String userInput = getInput();
             java.security.Security.setProperty("networkaddress.cache.ttl", "0");
-            String compoundNamePreHyphen = userInput.toLowerCase();
-            String compoundName = addHyphens(compoundNamePreHyphen);
+            String compoundName = addHyphens(userInput.toLowerCase());
             String urlString = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/" + compoundName + "/JSON";
-
-            System.out.println("[DEBUG] Sending out request.");
-
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-
-            int responseCode = conn.getResponseCode();
-            if (responseCode == 200) {
-                System.out.println("[DEBUG] Code 200! JSON Found!");
-                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                String inputLine;
-                StringBuilder response = new StringBuilder();
-                while ((inputLine = in.readLine()) != null){
-                    response.append(inputLine);
-                }
-                in.close();
-
-                System.out.println("[NOTIFICATION] Response from PubChem API:");
-                System.out.println(response.toString());
-
-                JsonObject jsonObject = JsonParser.parseString(response.toString()).getAsJsonObject();
-                System.out.println("[DEBUG] Parsing and sorting JSON.");
-
-                System.out.println("CID: " + jsonObject.getAsJsonArray("PC_Compounds")
-                        .get(0).getAsJsonObject().get("id"));
-
-                System.out.println("Label: " +
-                        jsonObject.getAsJsonArray("PC_Compounds")
-                                .get(0).getAsJsonObject().getAsJsonArray("props")
-                                .get(0).getAsJsonObject().getAsJsonObject("urn")
-                                .get("label").getAsString());
-
-            } else {
-                System.out.println("[ERROR] HTTP ERROR CODE: " + responseCode);
-                System.out.println("[ERROR] Did you put the wrong chemical in?");
-            }
-            conn.disconnect();
+            handleRequest(urlString);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public static void handleRequest(String urlString) throws Exception {
+        System.out.println("[DEBUG] Sending out request.");
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        int responseCode = conn.getResponseCode();
+        if (responseCode == 200) {
+            System.out.println("[DEBUG] Code 200! JSON Found!");
+            String response = readResponse(conn);
+            processResponse(response);
+        } else {
+            System.out.println("[ERROR] HTTP ERROR CODE: " + responseCode);
+            System.out.println("[ERROR] Did you put the wrong chemical in?");
+        }
+        conn.disconnect();
+    }
+
+    public static String readResponse(HttpURLConnection conn) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    public static void processResponse(String response) {
+        JsonObject jsonObject = JsonParser.parseString(response).getAsJsonObject();
+        System.out.println("[DEBUG] Parsing and sorting JSON.");
+        System.out.println("CID: " + jsonObject.getAsJsonArray("PC_Compounds")
+                .get(0).getAsJsonObject().get("id"));
+        System.out.println("Label: " +
+                jsonObject.getAsJsonArray("PC_Compounds")
+                        .get(0).getAsJsonObject().getAsJsonArray("props")
+                        .get(0).getAsJsonObject().getAsJsonObject("urn")
+                        .get("label").getAsString());
+    }
+
     public static String addHyphens(String text) {
         return text.replaceAll("\\s+", "-");
+    }
+
+    static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("[NOTIFICATION] Enter your query: ");
+        String userInput = scanner.nextLine();
+        scanner.close();
+        return userInput;
     }
 }
